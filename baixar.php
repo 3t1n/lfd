73b0610c44d98ece9577eab2c43d682989856d82
Reynaldo Tadeu Lourenço Mansi <reynaldotadeu10@gmail.com>
2020-06-04 12:44:18 -0300

    Update baixar.php

--- baixar.php
+++ baixar.php
@@ -1,29 +1,20 @@
 <?php      
        
-       //o script sem validar baixa o arquivo determinado pelo GET
-       //Porem não impedi a manipulação da URL feito pelo usuário.
- 
-       function download_arquivo($arquivo){
-        //Tamanho do arquivo em bytes.
-        $tamanho = filesize("$arquivo");
-        //Pegando extensão do arquivo.
-        $ext = explode (".",$arquivo);
-        //pega o nome do arquivo
-        $nome = $ext[0];
-        //Envia cabeçalhos HTTP  informações tipo, tamanho entre outras.
-        header("Content-Type: application/save");
-        header("Content-Length: $tamanho");
-        header("Content-Disposition: attachment; filename=$nome.$ext[1]");
-        header("Content-Transfer-Encoding: binary");
-        //Nesse momento envia o arquivo.
-        $fp = fopen("$arquivo", "r");
-        fpassthru($fp);
-        fclose($fp);
-       }
-       //Usando a function Dowload
-       download_arquivo($_GET['arquivo']);
- 
-     //ex: http://localhost:8080/Site/baixar.php?arquivo=tutorial.pdf
- 
- 
+//Script utilizado para baixar arquivos do meu site, como planilhas e pdf.
+
+function baixar_arquivo($arquivo){
+    $tamanho = filesize("$arquivo");
+    //Envia cabeçalhos HTTP  informações tipo, tamanho entre outras.
+    header("Content-Type: application/save");
+    header("Content-Length: $tamanho");
+    header("Content-Disposition: attachment; filename=$arquivo");
+    header("Content-Transfer-Encoding: binary");
+    //Nesse momento envia o arquivo.
+    $fp = fopen("$arquivo", "r");
+    fpassthru($fp);
+    fclose($fp);
+}
+//Percebam como eu recebo o GET do usuário e não realizo nenhum tratamento nele e ai que está nossa vulnerabilidade.
+baixar_arquivo($_GET['arquivo']);
+
 ?>
